147959d4fe532f53c5f4b2121dace97b43139800
dn00 <dtnguy90@gmail.com>
2017-02-27 04:40:39 -0800

    Update config.py

--- scripts/config.py
+++ scripts/config.py
@@ -7,7 +7,7 @@ def init_options():
     certid= '#'
     devid= '#'
     token= 'hwNcTIAnjr9EgiSb5HRjXvDZLFuQCFC7MePiOpTlgaXbpWfqV+djgVcC8QE6P3MMj1Sd2c6rKBmHrdx46G4bkh80cSQrBjRhJ6LzTQp2ls1LhgFXbZ7O9B6yWhkyCIP1WtrGGB0f+xwL/a/XQb2Dm4NlGLlAC7VliQC/6YSXT9cY5h4wPJQp7KxLnv9pQozT+4UxeYYbJHF8J2YWXzPXybiEE0+XIkbuj9n5fMeu52gGD/pvqw/Ps6Tm/twwHYW7zNHBWFSVSOMC06BqFUbVgnDp2X5xFL8nJtMrz4tgd/SvyVgGP3u+Crk4hl0jwYs7YWFM6ebVhjj6fcy3zYHMzacjbzv0P8vNQgTad3lka05FMz8II09dv1wVNxC4eJWIA9crMecNRH7nU1QKJMehrsETz9V3g3HDcg6j3L69skeSkl4R/6egXbvY8qIgTD+x+GfowIoIRHHjemHtzI5j+NajJBuSJCTvKqvLoykaZwAC+drKNuqdjBEPrzKRqgjEYLa4oPigOmIYxWdc2BFUJQRbze95GhxG+QBwaSt2GHkWWmWQQgcFC4LycKJAXLoOgseIqHmASTU5murp5T2V8BjbwSNeGzG90kQOxUNjIiP4BDsU4b78LtvdkbbJDx8AZ5oKPsUunUIYmZ8zLNByWM6xdrB3Ith+A8T'
-
+   # token isn't valid =P
     return appid, certid, devid, token
 
 #
